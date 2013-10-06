9d74821a9eb1b45f19a6c20306cccdfcfbe822c8
OKernel <okernelpro@me.com>
2013-10-06 10:48:03 +0800

    Step2

--- js/functions_dev.js
+++ js/functions_dev.js
@@ -92,10 +92,12 @@ function startHeartAnimation() {
 	};
 })(jQuery);
 
+//rebulid!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 function timeElapse(date){
+
 	var current = Date();
 	var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
-	var days = Math.floor(seconds / (3600 * 24));
+	var days = 	seconds / (3600 * 24);
 	seconds = seconds % (3600 * 24);
 	var hours = Math.floor(seconds / 3600);
 	if (hours < 10) {
@@ -113,7 +115,7 @@ function timeElapse(date){
 	var result = "<span class=\"digit\">" + days + "</span> days <span class=\"digit\">" + hours + "</span> hours <span class=\"digit\">" + minutes + "</span> minutes <span class=\"digit\">" + seconds + "</span> seconds"; 
 	$("#elapseClock").html(result);
 }
-
+//rebulid!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 function showMessages() {
 	adjustWordsPosition();
 	$('#messages').fadeIn(5000, function() {
